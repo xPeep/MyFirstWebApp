a89110e6c7a9dafec8af218a36b91b9de666dae0
josef-bohm <josefbohmjb@gmail.com>
2021-02-28 18:52:28 +0100

    add hello servelt

--- src/main/java/com/example/MyFirstWebApp/HelloServlet.java
+++ src/main/java/com/example/MyFirstWebApp/HelloServlet.java
@@ -1,10 +1,11 @@
 package com.example.MyFirstWebApp;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -12,14 +13,20 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        System.out.println("a POST request was sent to /hello");
+
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("a GET request was sent to /hello");
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Greetings!!!</h1>");
+        response.getWriter().println("<h3>Pickles are delicious</h3>");
+
     }
 
     public void destroy() {
